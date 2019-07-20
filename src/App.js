97774c57aafd03e9f7e34e460811c6ef5228b348
Chris Held <cheld100@gmail.com>
2019-07-20 11:11:06 -0500

    quick and dirty

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const Row = ({ reps, percentage, max }) => {
+  const weight = (percentage * max) / 100;
+  const rounded = Math.floor(weight / 5) * 5;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex-container">
+      <div>{reps}</div>
+      <div>{rounded}</div>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [max, setMax] = useState(100);
+  return (
+    <div className="root">
+      <div className="flex-container">
+        <div>TM</div>
+        <div>
+          <input
+            type="text"
+            value={max}
+            onChange={e => {
+              setMax(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+      <hr />
+      <Row reps={5} percentage={75} max={max} />
+      <Row reps={5} percentage={80} max={max} />
+      <Row reps={5} percentage={85} max={max} />
+      <hr />
+      <Row reps={3} percentage={80} max={max} />
+      <Row reps={3} percentage={85} max={max} />
+      <Row reps={3} percentage={90} max={max} />
+      <hr />
+      <Row reps={5} percentage={75} max={max} />
+      <Row reps={3} percentage={85} max={max} />
+      <Row reps={1} percentage={95} max={max} />
+      <hr />
+      <Row reps={5} percentage={60} max={max} />
+      <Row reps={5} percentage={65} max={max} />
+      <Row reps={5} percentage={70} max={max} />
+      <hr />
+    </div>
+  );
+};
 
 export default App;
